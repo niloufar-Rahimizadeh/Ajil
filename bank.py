ef2c6217272156507071eb3e01a2aad69e3826df
rasulalizadeh <mohammadr.alizadeh@live.com>
2021-07-11 14:04:30 +0430

    20Tir

--- bank.py
+++ bank.py
@@ -2,6 +2,11 @@ from tkinter import *
 from datetime import datetime
 
 # #########functions###########
+def press(label):
+    global counter_list
+    if counter_list:
+        con[label]['textvariable'].set(counter_list.pop(0))
+
 
 
 def get_date():
@@ -85,11 +90,9 @@ con = {
     },
     'waiting': {
         'textvariable': StringVar(),
-
     },
     'time': {
         'textvariable': StringVar(),
-
     },
     'date': {
         'textvariable': StringVar(),
@@ -114,9 +117,9 @@ operators.title("Operators")
 operators.geometry("600x200")
 operators.configure(bg="#e0f5d7")
 
-Button(operators, cnf=con['op1']).grid(row=0, column=0, padx=45, pady=25)
-Button(operators, cnf=con['op2']).grid(row=0, column=1, padx=45, pady=25)
-Button(operators, cnf=con['op3']).grid(row=0, column=2, padx=45, pady=25)
+Button(operators, cnf=con['op1'], command= lambda:press("lop1")).grid(row=0, column=0, padx=45, pady=25)
+Button(operators, cnf=con['op2'], command= lambda:press("lop2")).grid(row=0, column=1, padx=45, pady=25)
+Button(operators, cnf=con['op3'], command= lambda:press("lop3")).grid(row=0, column=2, padx=45, pady=25)
 
 Label(operators, cnf=con['lop1']).grid(row=1, column=0, padx=45, pady=25)
 Label(operators, cnf=con['lop2']).grid(row=1, column=1, padx=45, pady=25)
---
+++ test03.py
@@ -0,0 +1,6 @@
+list_name = ["Amir", "kurosh", "Paniz", "Mahan"]
+
+
+am = list_name.pop(0)
+print(list_name)
+print(am)
